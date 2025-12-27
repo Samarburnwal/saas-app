e3ab8a472f60b241f50e3b4133a3da0448f15ea3
Samarburnwal <samarburnwal@gmail.com>
2025-12-27 11:07:58 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Welcome to the Home Page</h1>
+      <Button>get started</Button>
+    </div>
+  )
+}
+
+export default Page
